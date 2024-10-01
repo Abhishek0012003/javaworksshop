f1d25efa9f5824c65b8a7fa7fc6160d5dd55f745
Abhishek0012003 <abhishekabhi09951@gmail.com>
2024-10-01 20:12:50 +0530

    pushing code

---
+++ TotalPrice.java
@@ -0,0 +1,20 @@
+public class TotalPric{
+
+           public static void main(String args[]){
+               String productName="Victor";
+               int gst = 18%;
+               int price=200000;
+               int numberOfProducts = ; 
+               int totalPrice = price/numberOfProducts;
+               System.out.println("product name:"+productName);
+               System.out.println("total price is:"+totalPrice);
+               System.out.println(")
+
+
+}
+
+
+
+
+
+}
